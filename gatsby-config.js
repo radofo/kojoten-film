5292693201f1835e94af82d59c1bb701e4b31bf1
Ramon Adofo <ramonadofo@hotmail.com>
2020-04-01 07:58:26 +0200

    Setup connection to Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,20 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_PREVIEW,
+        host: `preview.contentful.com`,
+        downloadLocal: true,
+      },
+    },
+  ],
 }
