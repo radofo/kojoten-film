4233e961e147e61aa0221d6b88da01c599ebab91
Ramon Adofo <ramonadofo@hotmail.com>
2020-08-11 08:44:52 +0200

    Fix Safari Image Stretch Bug

--- src/components/filmPoster.js
+++ src/components/filmPoster.js
@@ -1,19 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
 import { createSrcSet } from "../utils/fetch"
 
 const PosterImage = styled.img`
   height: 100%;
+  height: ${props => props.posterHeight}px;
   padding-top: var(--header-height);
 `
 
 const FilmPoster = ({ film }) => {
+  const [posterHeight, setPosterHeight] = useState(0)
+
+  useEffect(() => {
+    setPosterHeight(window.innerHeight)
+    window.addEventListener("resize", handleResize)
+  }, [])
+
+  const handleResize = () => {
+    setPosterHeight(window.innerHeight)
+  }
+
   const poster = film.fields.poster.fields
   const [posterSrc, posterSrcSet] = createSrcSet(poster.file.url)
   return (
     <Link to={`/film/${film.fields.url}`} state={{ project: film }}>
-      <PosterImage srcSet={posterSrcSet} src={posterSrc} alt={poster.title} />
+      <PosterImage
+        posterHeight={posterHeight}
+        srcSet={posterSrcSet}
+        src={posterSrc}
+        alt={poster.title}
+      />
     </Link>
   )
 }
